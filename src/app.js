826621ea4a43c02ba71287f134900a1c59fadd2f
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-10-12 13:10:57 -0600

    :memo: Add or update documentation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 // MODULEJS: import from || export default
-
 // COMMOMJS: require || module.export
 
 import express from "express";
+import pool from "./server.js";
 
 const app = express();
 
@@ -10,6 +10,11 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await pool.query("SELECT NOW() ");
+  return res.json(users.rows[0]);
 });
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
+import { config } from "dotenv";
 import app from "./app.js";
+import pg from "pg";
+
+config();
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 const PORT = process.env.PORT_SERVER || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} 🦕 `);
 });
+
+export default pool;
